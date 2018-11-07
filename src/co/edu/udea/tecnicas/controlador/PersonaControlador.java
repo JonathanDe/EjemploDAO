312cd26a011b1c9a8182ebee5489ba28817927a2
Brian Vanegas <brianvanegas@viajesaeromar.com>
2018-11-06 20:02:36 -0500

    formulario fx almacenar persona

--- src/co/edu/udea/tecnicas/controlador/PersonaControlador.java
+++ src/co/edu/udea/tecnicas/controlador/PersonaControlador.java
@@ -1,14 +1,25 @@
 package co.edu.udea.tecnicas.controlador;
 
-import java.util.List;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.text.Text;
+import javafx.scene.control.*;
 
+import java.util.List;
 import co.edu.udea.tecnicas.modelo.PersonaDTO;
 import co.edu.udea.tecnicas.negocio.PersonaNegocio;
 
 public class PersonaControlador {
+    @FXML private Text actionStatus;
+    @FXML private TextField documento;
+    @FXML private TextField nombres;
+    @FXML private TextField apellidos;
+    @FXML private TextField genero;
+    
     PersonaNegocio personaNegocio = new PersonaNegocio();
 
-    public boolean almacenarPersona(PersonaDTO persona) { // pudo haber sido
+    public boolean almacenarPersona(PersonaDTO persona) {
+        
         // invocado por un
         // evento de un
         // bot�n
@@ -29,6 +40,28 @@ public class PersonaControlador {
     
     public boolean eliminarPersona(String identificacion){
         return personaNegocio.eliminarPersona(identificacion);
+    }
+    
+    /**
+     *
+     * @param event
+     * Boton Almacenar Persona
+     */
+    @FXML protected void handleSubmitAlmacenarPersona(ActionEvent event) {
+        if (!nombres.getText().equals("") && !apellidos.getText().equals("") && !genero.getText().equals("") && !documento.getText().equals("")) {
+            PersonaDTO persona = new PersonaDTO(nombres.getText(), apellidos.getText(), genero.getText().charAt(0), documento.getText());
+            if(this.almacenarPersona(persona)){
+                actionStatus.setText("Usuario registrado con éxito!");
+                nombres.setText("");
+                apellidos.setText("");
+                genero.setText("");
+                documento.setText("");
+            } else {
+                actionStatus.setText("Ocurrió un problema ingresando el usuario!");
+            }
+        } else {
+            actionStatus.setText("El formulario no puede ser vacío!");
+        }
     }
 
 }
